c5eca02c082ad831e791c12ac9206d7e719b4dae
lozzbyt-lab <lozzbyt@gmail.com>
2026-01-09 17:01:52 +0530

    Update vite.config.ts
    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "jpskating.onrender.com",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
